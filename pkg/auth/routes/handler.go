d5c765c43646b8008bf657d6c364a4e9c69ea978
Darmoriz <ihor.darmoriz@sigmaukraine.com>
2021-09-10 15:59:11 +0300

    add auth server

---
+++ pkg/auth/routes/handler.go
@@ -0,0 +1,85 @@
+package routes
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"monorepa/model"
+	"monorepa/service/auth"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type HandlerItemsServ struct {
+	router   *mux.Router
+	services auth.AuthService
+	ctx      context.Context
+}
+
+func New(ctx context.Context, router *mux.Router) *HandlerItemsServ {
+	return &HandlerItemsServ{
+		router:   router,
+		services: auth.New(),
+		ctx:      ctx,
+	}
+}
+
+func (h *HandlerItemsServ) HandlerItems() {
+	h.router.HandleFunc("/login", h.GetJWTToken).Methods("GET")
+	h.router.HandleFunc("/get-cert/{version}", h.GetCertKey).Methods("GET")
+}
+
+func (h *HandlerItemsServ) GetJWTToken(w http.ResponseWriter, r *http.Request) {
+	var user model.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	token, err := h.services.Login(user)
+	if err != nil || token == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("token", token)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *HandlerItemsServ) GetCertKey(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	versionCert := params["version"]
+	if versionCert == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	byteCertPbKey, err := h.services.GetCert(versionCert)
+	if err != nil {
+		fmt.Println(err)
+
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// resp struct with token
+	// decode and return json
+	type tokenResp struct {
+		PbKey []byte `json:"publicKey"`
+	}
+
+	resp := tokenResp{
+		PbKey: byteCertPbKey,
+	}
+
+	respByte, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(respByte)
+	w.WriteHeader(http.StatusOK)
+}
