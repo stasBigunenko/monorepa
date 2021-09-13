1010121a7869443f5a2664e63147c3487bb435ab
Darmoriz <ihor.darmoriz@sigmaukraine.com>
2021-09-13 14:11:24 +0300

    add transport layer

---
+++ pkg/auth/routes/handler_test.go
@@ -0,0 +1,262 @@
+package routes
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"monorepa/model"
+	"monorepa/service/auth"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestUserTokenGen(t *testing.T) {
+	// test info
+	type request struct {
+		endpoint string
+		method   string
+		body     func() []byte
+	}
+
+	type wantResp struct {
+		code        int
+		headerToken bool
+		body        func() []byte
+	}
+
+	testCases := []struct {
+		name            string
+		service         *auth.MockAuthService // service to mock
+		serviceFuncResp func(*auth.MockAuthService, []byte)
+		request         request
+		want            wantResp
+	}{
+		{
+			name:    "Normal request",
+			service: &auth.MockAuthService{},
+			serviceFuncResp: func(mc *auth.MockAuthService, items []byte) {
+				mc.On("Login",
+					mock.Anything,
+				).Return("qwerty.qwerty.qwerty", nil)
+			},
+			request: request{
+				endpoint: "/login",
+				method:   "GET",
+				body: func() []byte {
+					user := model.User{
+						Name:     "Bob",
+						Password: "12345",
+					}
+					res, _ := json.Marshal(user)
+					return res
+				},
+			},
+			want: wantResp{
+				code: 201,
+				body: func() []byte {
+					res, _ := json.Marshal(nil)
+					return res
+				},
+				headerToken: true,
+			},
+		},
+		{
+			name:    "Wirhout user data",
+			service: &auth.MockAuthService{},
+			serviceFuncResp: func(mc *auth.MockAuthService, items []byte) {
+				mc.On("Login",
+					mock.Anything,
+				).Return("", errors.New("wrong password"))
+			},
+			request: request{
+				endpoint: "/login",
+				method:   "GET",
+				body: func() []byte {
+					res, _ := json.Marshal(nil)
+					return res
+				},
+			},
+			want: wantResp{
+				code: 400,
+				body: func() []byte {
+					res, _ := json.Marshal(nil)
+					return res
+				},
+				headerToken: false,
+			},
+		},
+		{
+			name:    "Wirhout user password",
+			service: &auth.MockAuthService{},
+			serviceFuncResp: func(mc *auth.MockAuthService, items []byte) {
+				mc.On("Login",
+					mock.Anything,
+				).Return("", errors.New("wrong password"))
+			},
+			request: request{
+				endpoint: "/login",
+				method:   "GET",
+				body: func() []byte {
+					user := model.User{
+						Name: "Bob",
+					}
+					res, _ := json.Marshal(user)
+					return res
+				},
+			},
+			want: wantResp{
+				code: 400,
+				body: func() []byte {
+					res, _ := json.Marshal(nil)
+					return res
+				},
+
+				headerToken: false,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+
+		// init handler
+		handler := HandlerItemsServ{
+			router:   mux.NewRouter(),
+			ctx:      context.Background(),
+			services: tc.service,
+		}
+
+		tc.serviceFuncResp(tc.service, tc.request.body()) // mock internal function
+		handler.HandlerItems()
+
+		// server
+		hts := httptest.NewServer(handler.router)
+		defer hts.Close()
+
+		// client
+		cl := hts.Client()
+		req, _ := http.NewRequest(tc.request.method, hts.URL+tc.request.endpoint, bytes.NewReader(tc.request.body()))
+
+		// request proces
+		res, err := cl.Do(req)
+		if err != nil {
+			t.Error("request error :", err)
+		}
+
+		// results
+		assert.Equal(t, tc.want.code, res.StatusCode, tc.name)
+
+		token := res.Header.Get("token")
+		if tc.want.headerToken {
+			assert.NotEmpty(t, token, tc.name)
+		} else {
+			assert.Empty(t, token, tc.name)
+		}
+	}
+}
+
+func TestGetCertificateKey(t *testing.T) {
+
+	type tokenResp struct {
+		PbKey []byte `json:"publicKey"`
+	}
+
+	// test info
+	type request struct {
+		endpoint string
+		method   string
+	}
+
+	type wantResp struct {
+		code   int
+		isBody bool
+	}
+
+	testCases := []struct {
+		name            string
+		service         *auth.MockAuthService // service to mock
+		serviceFuncResp func(*auth.MockAuthService)
+		request         request
+		want            wantResp
+	}{
+		{
+			name:    "Normal request",
+			service: &auth.MockAuthService{},
+			serviceFuncResp: func(mc *auth.MockAuthService) {
+				mc.On("GetCert",
+					mock.Anything,
+				).Return([]byte("qwerty.qwerty.qwerty"), nil)
+			},
+			request: request{
+				endpoint: "/get-cert/1",
+				method:   "GET",
+			},
+			want: wantResp{
+				code:   200,
+				isBody: true,
+			},
+		},
+		{
+			name:    "Internal error",
+			service: &auth.MockAuthService{},
+			serviceFuncResp: func(mc *auth.MockAuthService) {
+				mc.On("GetCert",
+					mock.Anything,
+				).Return([]byte{}, errors.New("some err"))
+			},
+			request: request{
+				endpoint: "/get-cert/foo=bar&baz=bar",
+				method:   "GET",
+			},
+			want: wantResp{
+				code:   500,
+				isBody: false,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+
+		// init handler
+		handler := HandlerItemsServ{
+			router:   mux.NewRouter(),
+			ctx:      context.Background(),
+			services: tc.service,
+		}
+
+		tc.serviceFuncResp(tc.service) // mock internal function
+		handler.HandlerItems()
+
+		// server
+		hts := httptest.NewServer(handler.router)
+		defer hts.Close()
+
+		// client
+		cl := hts.Client()
+		req, _ := http.NewRequest(tc.request.method, hts.URL+tc.request.endpoint, nil)
+
+		// request proces
+		res, err := cl.Do(req)
+		if err != nil {
+			t.Error("request error :", err)
+		}
+
+		// results
+		assert.Equal(t, tc.want.code, res.StatusCode, tc.name)
+
+		if tc.want.isBody {
+			var respData tokenResp
+			err = json.NewDecoder(res.Body).Decode(&respData)
+			assert.Nil(t, err, tc.name)
+
+			assert.NotEmpty(t, respData.PbKey, tc.name)
+		} else {
+			assert.Empty(t, res.Body, tc.name)
+		}
+	}
+}
--- pkg/auth/server.go
+++ pkg/auth/server.go
@@ -1,1 +1,98 @@
-package server
+package auth
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"monorepa/pkg/auth/middleware"
+	"monorepa/pkg/auth/routes"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	ctx    context.Context
+	config struct { // can be used as general model
+		Host string
+		Port string
+	}
+	router *mux.Router
+}
+
+func New(ctx context.Context) *Server {
+	return &Server{
+		router: mux.NewRouter(),
+		ctx:    ctx,
+	}
+}
+
+func (s *Server) ServerAddrConfig() error {
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		return errors.New("wrong port")
+	}
+
+	s.config.Host = os.Getenv("SERVER_HOST")
+	s.config.Port = port
+	return nil
+}
+
+/**
+ * all routes of http server
+ * also contain connection to graphql server
+ */
+
+func (s *Server) GetRouters() {
+	itemsHandler := routes.New(s.ctx, s.router)
+	itemsHandler.HandlerItems()
+}
+
+func (s *Server) getHttpAddress() string {
+	return fmt.Sprintf("%s:%s", s.config.Host, s.config.Port)
+}
+
+func (s *Server) Start(ctx context.Context) error {
+	// init server
+	server := &http.Server{
+		Addr:         s.getHttpAddress(),
+		Handler:      middleware.JsonRespHeaders(s.router),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	cancelTimeout, errTimeout := strconv.Atoi(os.Getenv("Server_Cancel_Timeout"))
+	if errTimeout != nil {
+		return errTimeout
+	}
+
+	// run server
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	fmt.Println("Server is running on: " + s.getHttpAddress())
+
+	<-ctx.Done() // wait end of work
+
+	/**
+	 * start graceful shutdown
+	 */
+
+	log.Println("Server stopped")
+
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), time.Duration(cancelTimeout)*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctxShutDown); err != nil {
+		return err
+	}
+
+	log.Printf("Server exited properly")
+	return nil
+}
