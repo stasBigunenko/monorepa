6a2d4523ffd34279ae1e25f9817859f24958f62f
Alex A. Renoire <aleksandra.alekseeva@sigma.software>
2021-09-17 13:13:05 +0300

    added models for account and userHTTP

---
+++ model/account.go
@@ -0,0 +1,9 @@
+package model
+
+import "github.com/google/uuid"
+
+type Account struct {
+	ID      uuid.UUID `json:"id,omitempty"`
+	UserID  uuid.UUID `json:"user_id,omitempty"`
+	Balance int       `json:"balance,omitempty"`
+}
