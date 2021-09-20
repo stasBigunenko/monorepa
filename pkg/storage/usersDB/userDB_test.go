add7629f10938f2f52f6eea3ded80aad739cf574
stanislav.bigunenko <stas.bigunenko@gmail.com>
2021-09-20 07:34:24 +0300

    Small fixes

--- pkg/storage/usersDB/userDB_test.go
+++ pkg/storage/usersDB/userDB_test.go
@@ -53,55 +53,55 @@ func TestUserDB(t *testing.T) {
 	})
 }
 func TestModificateUser(t *testing.T) {
-	us := NewUserStorage()
+	us2 := NewUserStorage()
 
 	id1 := uuid.New()
 	user1 := model.UserHTTP{
 		ID:   id1,
 		Name: "Jack",
 	}
-	us.MapUserHTTP[id1] = user1
+	us2.MapUserHTTP[id1] = user1
 
 	id2 := uuid.New()
 	user2 := model.UserHTTP{
 		ID:   id2,
 		Name: "Johnie",
 	}
-	us.MapUserHTTP[id2] = user2
+	us2.MapUserHTTP[id2] = user2
 
 	id3 := uuid.New()
 	user3 := model.UserHTTP{
 		ID:   id3,
 		Name: "Jim",
 	}
-	us.MapUserHTTP[id3] = user3
+	us2.MapUserHTTP[id3] = user3
 
 	t.Run("Update", func(t *testing.T) {
 		user2.Name = "Glen"
 
-		val, _ := us.Update(context.Background(), user2)
+		val, _ := us2.Update(context.Background(), user2)
 		require.Equal(t, user2, val, "should update account according id")
 
 		errAccount := model.UserHTTP{
 			ID:   uuid.New(),
 			Name: "Empty",
 		}
-		_, err := us.Update(context.Background(), errAccount)
+		_, err := us2.Update(context.Background(), errAccount)
 		require.Errorf(t, err, "should be error not found")
 	})
 
 	t.Run("Delete=", func(t *testing.T) {
 		delUser := user1
-		us.Delete(context.Background(), delUser.ID)
-		require.Empty(t, model.Account{})
+		err := us2.Delete(context.Background(), delUser.ID)
+		require.Empty(t, err)
 
-		err := us.Delete(context.Background(), delUser.ID)
+		err = us2.Delete(context.Background(), delUser.ID)
 		require.Errorf(t, err, "should be not found")
 	})
 }
 
 func TestAccountStorage_CreateAccount(t *testing.T) {
-	us := NewUserStorage()
-	val, _ := us.Create(context.Background(), "newMan")
+	us3 := NewUserStorage()
+	val, _ := us3.Create(context.Background(), "newMan")
 	require.NotNil(t, val, "should be no error")
 }
