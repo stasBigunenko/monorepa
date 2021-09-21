a6d79939882ff560c6e0f3512abbb476631e6983
stanislav.bigunenko <stas.bigunenko@gmail.com>
2021-09-21 19:33:27 +0300

    Add user service with tests, added mock on storage

---
+++ service/user/service.go
@@ -0,0 +1,118 @@
+package user
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/stasBigunenko/monorepa/pkg/storage/newStorage"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"unicode"
+
+	"github.com/google/uuid"
+	"github.com/stasBigunenko/monorepa/model"
+)
+
+type UserService struct {
+	Us      model.UserHTTP
+	storage newStorage.NewStore
+}
+
+func NewUserService(s newStorage.NewStore) *UserService {
+	return &UserService{
+		storage: s,
+	}
+}
+
+func (u *UserService) Get(_ context.Context, id uuid.UUID) (model.UserHTTP, error) {
+
+	res, err := u.storage.Get(context.Background(), id)
+	if res == nil && err == nil || err != nil {
+		return model.UserHTTP{}, status.Error(codes.Internal, "internal problem")
+	}
+
+	err = json.Unmarshal(res, &u.Us)
+	if err != nil {
+		return model.UserHTTP{}, status.Error(codes.Aborted, "wrong with unmarshal")
+	}
+
+	return u.Us, nil
+}
+
+func (u *UserService) GetAll(_ context.Context) ([]model.UserHTTP, error) {
+
+	res, err := u.storage.GetAll(context.Background())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "storage problem")
+	}
+
+	ac := []model.UserHTTP{}
+
+	for _, val := range res {
+		err := json.Unmarshal(val, &u.Us)
+		if err != nil {
+			return nil, status.Error(codes.Internal, "internal problem")
+		}
+
+		ac = append(ac, u.Us)
+	}
+	return ac, nil
+}
+
+func (u *UserService) Create(_ context.Context, b string) (model.UserHTTP, error) {
+
+	for _, r := range b {
+		if !unicode.IsLetter(r) {
+			return model.UserHTTP{}, status.Error(codes.InvalidArgument, "invalid username")
+		}
+	}
+
+	if len(b) <= 2 {
+		return model.UserHTTP{}, status.Error(codes.InvalidArgument, "invalid name")
+	}
+
+	bt, err := json.Marshal(b)
+	if err != nil {
+		return model.UserHTTP{}, status.Error(codes.Internal, "internal problem")
+	}
+
+	res, id, err := u.storage.Create(context.Background(), bt)
+	if err != nil {
+		return model.UserHTTP{}, status.Error(codes.Internal, "internal problem")
+	}
+
+	err = json.Unmarshal(res, &u.Us)
+
+	u.Us.ID = id
+
+	return u.Us, nil
+}
+
+func (u *UserService) Update(_ context.Context, user model.UserHTTP) (model.UserHTTP, error) {
+	id := user.ID
+
+	bt, err := json.Marshal(user)
+	if err != nil {
+		return model.UserHTTP{}, status.Error(codes.Internal, "internal problem")
+	}
+
+	res, err := u.storage.Update(context.Background(), id, bt)
+	if err != nil || res == nil && err == nil {
+		return model.UserHTTP{}, status.Error(codes.Internal, "internal problem")
+	}
+
+	err = json.Unmarshal(res, &u.Us)
+	if err != nil {
+		return model.UserHTTP{}, status.Error(codes.Internal, "internal problem")
+	}
+
+	return u.Us, nil
+}
+
+func (u *UserService) Delete(_ context.Context, id uuid.UUID) error {
+	b, err := u.storage.Delete(context.Background(), id)
+	if err != nil || b == false {
+		return status.Error(codes.Internal, "not found")
+	}
+
+	return nil
+}
---
+++ service/user/userService_test.go
@@ -0,0 +1,243 @@
+package user
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/google/uuid"
+	"github.com/stasBigunenko/monorepa/mocks/pkg/storage/mockNewStore"
+	"github.com/stasBigunenko/monorepa/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"testing"
+)
+
+func Test_Create(t *testing.T) {
+	ui := new(mockNewStore.NewStore)
+	id := uuid.New()
+	m := model.UserHTTP{ID: id, Name: "Andrew"}
+	b, _ := json.Marshal("Andrew")
+	mj, _ := json.Marshal(m)
+	ui.On("Create", context.Background(), b).Return(mj, id, nil)
+
+	ui2 := new(mockNewStore.NewStore)
+	ui2.On("Create", context.Background(), "123").Return(nil, nil)
+
+	ui3 := new(mockNewStore.NewStore)
+	ui3.On("Create", context.Background(), "S").Return(nil, nil)
+
+	tests := []struct {
+		name    string
+		param   string
+		stor    *mockNewStore.NewStore
+		want    model.UserHTTP
+		wantErr codes.Code
+	}{
+		{
+			name:    "Everything good",
+			param:   "Andrew",
+			stor:    ui,
+			want:    model.UserHTTP{ID: id, Name: "Andrew"},
+			wantErr: codes.OK,
+		},
+		{
+			name:    "Everything good",
+			param:   "12",
+			stor:    ui2,
+			want:    model.UserHTTP{},
+			wantErr: codes.InvalidArgument,
+		},
+		{
+			name:    "Everything good",
+			param:   "S",
+			stor:    ui3,
+			want:    model.UserHTTP{},
+			wantErr: codes.InvalidArgument,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			u := NewUserService(tc.stor)
+			got, err := u.Create(context.Background(), tc.param)
+			if (err != nil) && status.Code(err) != tc.wantErr {
+				t.Errorf("error = %v, wantErr %v", err.Error(), tc.wantErr)
+				return
+			}
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func Test_Get(t *testing.T) {
+	ui := new(mockNewStore.NewStore)
+	id := uuid.New()
+	m := model.UserHTTP{ID: id, Name: "Andrew"}
+	mj, _ := json.Marshal(m)
+	ui.On("Get", context.Background(), id).Return(mj, nil)
+
+	ui2 := new(mockNewStore.NewStore)
+	ui2.On("Get", mock.Anything, mock.Anything).Return(nil, nil)
+
+	tests := []struct {
+		name    string
+		stor    *mockNewStore.NewStore
+		want    model.UserHTTP
+		wantErr codes.Code
+	}{
+		{
+			name: "Everything ok",
+			stor: ui,
+			want: model.UserHTTP{
+				ID:   id,
+				Name: "Andrew",
+			},
+			wantErr: codes.OK,
+		},
+		{
+			name:    "Everything ok",
+			stor:    ui2,
+			want:    model.UserHTTP{},
+			wantErr: codes.Internal,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			u := NewUserService(tc.stor)
+			got, err := u.Get(context.Background(), id)
+			if err != nil && status.Code(err) != tc.wantErr {
+				t.Errorf("error = %v, wantErr %v", err.Error(), tc.wantErr)
+				return
+			}
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestUserService_Delete(t *testing.T) {
+	ui := new(mockNewStore.NewStore)
+	id := uuid.New()
+	ui.On("Delete", context.Background(), id).Return(true, nil)
+
+	ui2 := new(mockNewStore.NewStore)
+	ui2.On("Delete", mock.Anything, mock.Anything).Return(false, nil)
+
+	tests := []struct {
+		name    string
+		stor    *mockNewStore.NewStore
+		param   uuid.UUID
+		result  []byte
+		wantErr codes.Code
+	}{
+		{
+			name:    "Everything ok",
+			stor:    ui,
+			param:   id,
+			wantErr: codes.OK,
+		},
+		{
+			name:    "false",
+			stor:    ui2,
+			wantErr: codes.Internal,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			u := NewUserService(tc.stor)
+			err := u.Delete(context.Background(), tc.param)
+			if err != nil {
+				assert.Error(t, err)
+				return
+			}
+			assert.Empty(t, err)
+		})
+	}
+}
+
+//
+func TestUserService_GetAll(t *testing.T) {
+	ui := new(mockNewStore.NewStore)
+	m1 := model.UserHTTP{ID: uuid.New(), Name: "Andrew"}
+	m2 := model.UserHTTP{ID: uuid.New(), Name: "Ivan"}
+	m := []model.UserHTTP{
+		m1,
+		m2,
+	}
+	var mj [][]byte
+
+	for _, val := range m {
+		j, _ := json.Marshal(val)
+		mj = append(mj, j)
+	}
+	ui.On("GetAll", context.Background()).Return(mj, nil)
+
+	tests := []struct {
+		name    string
+		stor    *mockNewStore.NewStore
+		want    []model.UserHTTP
+		wantErr codes.Code
+	}{
+		{
+			name:    "Everything ok",
+			stor:    ui,
+			want:    m,
+			wantErr: codes.OK,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			u := NewUserService(tc.stor)
+			got, err := u.GetAll(context.Background())
+			if (err != nil) && status.Code(err) != tc.wantErr {
+				t.Errorf("error = %v, wantErr %v", err.Error(), tc.wantErr)
+				return
+			}
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestUserService_Update(t *testing.T) {
+	ui := new(mockNewStore.NewStore)
+	id := uuid.New()
+	m := model.UserHTTP{ID: id, Name: "Abdula"}
+	mj, _ := json.Marshal(m)
+	ui.On("Update", context.Background(), id, mj).Return(mj, nil)
+
+	ui2 := new(mockNewStore.NewStore)
+	ui2.On("Update", mock.Anything, mock.Anything, mock.Anything).Return(nil, nil)
+
+	tests := []struct {
+		name    string
+		stor    *mockNewStore.NewStore
+		param   model.UserHTTP
+		want    model.UserHTTP
+		wantErr codes.Code
+	}{
+		{
+			name:    "Everything ok",
+			stor:    ui,
+			param:   m,
+			want:    m,
+			wantErr: codes.OK,
+		},
+		{
+			name:    "Not found",
+			stor:    ui2,
+			param:   m,
+			want:    model.UserHTTP{},
+			wantErr: codes.Internal,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			u := NewUserService(tc.stor)
+			got, err := u.Update(context.Background(), m)
+			if (err != nil) && status.Code(err) != tc.wantErr {
+				t.Errorf("SomeLogic error = %v, wantErr %v", err.Error(), tc.wantErr)
+				return
+			}
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
