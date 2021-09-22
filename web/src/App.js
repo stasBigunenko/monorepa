b574bf51e5c7ff89baab51d824a20d7318968e48
Darmoriz <ihor.darmoriz@sigmaukraine.com>
2021-09-22 12:43:31 +0300

    update login

--- web/src/App.js
+++ web/src/App.js
@@ -20,7 +20,10 @@ class App extends React.Component {
   render(){
     console.log("state", this.state)
     return (
-      <Login handleUserLogin={this.onLogin}/>
+      <div>
+        <Login handleUserLogin={this.onLogin}/>
+        <hr/>
+      </div>
     );
   }
 }
